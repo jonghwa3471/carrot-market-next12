e2911037be7ccaf4f4370b72d2ec05f2ccd49e9c
Jonghwa <whdghk3471@gmail.com>
2025-05-11 11:27:16 +0900

    #2.8 More Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,14 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 p-5">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="peer rounded border border-gray-400 p-1"
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        Awesome username
-      </span>
-      <span className="hidden peer-hover:block peer-hover:text-amber-500">
-        Hi!
-      </span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    <div className="flex flex-col space-y-2 p-5">
+      <p className="first-line:text-7xl first-line:hover:text-purple-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non asperiores
+        sed, obcaecati nesciunt sapiente consectetur sunt molestiae est
+        molestias ut numquam blanditiis. Accusamus non harum, suscipit
+        distinctio laboriosam dolore iusto?
+      </p>
+    </div>
   );
 };
 
