11b3e2b9b6d5fb9013a01d2084466681b22b3d02
Jonghwa <whdghk3471@gmail.com>
2025-05-27 10:22:14 +0900

    #5.5 Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -7,13 +7,20 @@ interface LoginForm {
 }
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
   const onValid = (data: LoginForm) => {
     console.log("im valid baby");
   };
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
   };
+  console.log(errors);
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
@@ -30,10 +37,16 @@ export default function Forms() {
       <input
         {...register("email", {
           required: "Email is required",
+          validate: {
+            notGamil: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
         })}
         type="email"
         placeholder="Email"
+        className={`${Boolean(errors.email) ? "border-red-500" : ""}`}
       />
+      {errors.email?.message}
       <input
         {...register("password", {
           required: "Password is required",
