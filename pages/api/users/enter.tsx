604c65403af98185b581ebd7fd9d7cadf4b34331
Jonghwa <whdghk3471@gmail.com>
2025-05-29 22:01:57 +0900

    #6.4 withHandler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,14 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
+import withHandler from "../../../libs/server/withHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method !== "POST") {
-    res.status(405).end();
-    // 405 means current method is not allowed
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  res.status(200).end();
+  return res.status(200).end();
 }
+
+export default withHandler("POST", handler);
