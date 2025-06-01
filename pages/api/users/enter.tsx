9e7705548ea86c50bddc8f3eae03cb66d9feca71
Jonghwa <whdghk3471@gmail.com>
2025-06-01 21:58:19 +0900

    #7.5 Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+
+mail.setApiKey(process.env.SENDGRID_KEY!);
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -72,6 +75,14 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "whdghk3471@gmail.com",
+      to: "whdghk3471@gmail.com",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<stong>Your token is ${payload}</strong>`,
+    });
   }
   return res.json({
     ok: true,
