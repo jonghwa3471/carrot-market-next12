e53838addffecd110955db35151ca880f65f5346
Jonghwa <whdghk3471@gmail.com>
2025-06-27 10:56:46 +0900

    add: status

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -24,7 +24,7 @@ export default function withHandler({
       return res.status(405).end();
     }
     if (isPrivate && !req.session.user) {
-      return res.json({ ok: false, error: "Plz log in." });
+      return res.status(401).json({ ok: false, error: "Plz log in." });
     }
     try {
       await handler(req, res);
