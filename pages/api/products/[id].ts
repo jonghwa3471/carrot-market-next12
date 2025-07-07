f62bc44a53b01db094c52bc1ab509bb501ef3c33
Jonghwa <whdghk3471@gmail.com>
2025-07-07 23:30:19 +0900

    #9.5 Related Products

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -24,15 +24,15 @@ async function handler(
       },
     }); */
     const product = {
-      id: 1,
-      name: "상품1",
+      id,
+      name: `상품${id}`,
       price: "100$",
-      description: "상품 예시111",
+      description: `상품 예시${id}`,
       image: "xx",
       createdAt: "2025-01-26T08:24:50.545z",
       updatedAt: "2025-01-26T08:24:50.546z",
       user: {
-        id: 19,
+        id: 2 * +id,
         phone: "12345",
         email: null,
         name: "Anonymous",
@@ -41,7 +41,78 @@ async function handler(
         updatedAt: "2022-01-26T08:24:50.546z",
       },
     };
-    res.json({ ok: true, product });
+    const terms = product.name.split(" ").map((word) => ({
+      name: {
+        contains: word,
+      },
+    }));
+    /*     const relatedProducts = await client.product.findMany({
+      where: {
+        OR: terms,
+        AND: {
+          id: {
+            not: product?.id,
+          },
+        },
+      },
+    }); */
+    const relatedProducts = [
+      {
+        id: 3,
+        name: "상품3",
+        price: "300$",
+        description: "상품 예시333",
+        image: "xx",
+        createdAt: "2025-01-26T08:24:50.545z",
+        updatedAt: "2025-01-26T08:24:50.546z",
+        user: {
+          id: 39,
+          phone: "1234567",
+          email: null,
+          name: "Anonymous",
+          avatar: null,
+          createdAt: "2022-01-26T08:24:50.545z",
+          updatedAt: "2022-01-26T08:24:50.546z",
+        },
+      },
+      {
+        id: 4,
+        name: "상품4",
+        price: "400$",
+        description: "상품 예시444",
+        image: "xx",
+        createdAt: "2025-01-26T08:24:50.545z",
+        updatedAt: "2025-01-26T08:24:50.546z",
+        user: {
+          id: 49,
+          phone: "123456789",
+          email: null,
+          name: "Anonymous",
+          avatar: null,
+          createdAt: "2022-01-26T08:24:50.545z",
+          updatedAt: "2022-01-26T08:24:50.546z",
+        },
+      },
+      {
+        id: 5,
+        name: "상품5",
+        price: "300$",
+        description: "상품 예시555",
+        image: "xx",
+        createdAt: "2025-01-26T08:24:50.545z",
+        updatedAt: "2025-01-26T08:24:50.546z",
+        user: {
+          id: 59,
+          phone: "123456712",
+          email: null,
+          name: "Anonymous",
+          avatar: null,
+          createdAt: "2022-01-26T08:24:50.545z",
+          updatedAt: "2022-01-26T08:24:50.546z",
+        },
+      },
+    ];
+    res.json({ ok: true, product, relatedProducts });
   }
 }
 
