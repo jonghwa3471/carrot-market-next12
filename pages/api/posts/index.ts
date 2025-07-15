316becd7ef4e976685677375ef686842b7987417
Jonghwa <whdghk3471@gmail.com>
2025-07-15 18:02:27 +0900

    #10.5 All Posts

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -11,7 +11,8 @@ async function handler(
     body: { question },
     session: { user },
   } = req;
-  /*   const post = await client.post.create({
+  if (req.method === "POST") {
+    /*   const post = await client.post.create({
     data: {
       question,
       user: {
@@ -21,18 +22,110 @@ async function handler(
       },
     },
   }); */
-  const post = {
-    id: 1,
-    createdAt: "2022-01-26T08:24:50.545z",
-    updatedAt: "2022-01-26T08:24:50.546z",
-    user: user,
-    userId: user?.id,
-    question,
-  };
-  res.json({
-    ok: true,
-    post,
-  });
+    const post = {
+      id: 1,
+      createdAt: "2022-01-26T08:24:50.545z",
+      updatedAt: "2022-01-26T08:24:50.546z",
+      user: user,
+      userId: user?.id,
+      question,
+    };
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === "GET") {
+    /*     const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wonderings: true,
+            answers: true,
+          },
+        },
+      },
+    }); */
+    const posts = [
+      {
+        id: 1,
+        createdAt: "2022-01-26T08:24:50.545z",
+        updatedAt: "2022-01-26T08:24:50.546z",
+        user: {
+          id: 4,
+          name: "userName",
+          avatar: "",
+        },
+        userId: 123123,
+        question: "Question1",
+        _count: {
+          wondering: 7,
+          answers: 2,
+        },
+      },
+      {
+        id: 2,
+        createdAt: "2022-01-26T08:24:50.545z",
+        updatedAt: "2022-01-26T08:24:50.546z",
+        user: {
+          id: 4,
+          name: "userName",
+          avatar: "",
+        },
+        userId: 234,
+        question: "Question2",
+        _count: {
+          wondering: 7,
+          answers: 2,
+        },
+      },
+      {
+        id: 3,
+        createdAt: "2022-01-26T08:24:50.545z",
+        updatedAt: "2022-01-26T08:24:50.546z",
+        user: {
+          id: 4,
+          name: "userName",
+          avatar: "",
+        },
+        userId: 5345,
+        question: "Question3",
+        _count: {
+          wondering: 7,
+          answers: 2,
+        },
+      },
+      {
+        id: 4,
+        createdAt: "2022-01-26T08:24:50.545z",
+        updatedAt: "2022-01-26T08:24:50.546z",
+        user: {
+          id: 4,
+          name: "userName",
+          avatar: "",
+        },
+        userId: 45645645,
+        question: "Question4",
+        _count: {
+          wondering: 7,
+          answers: 2,
+        },
+      },
+    ];
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ["POST"], handler }));
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler }),
+);
