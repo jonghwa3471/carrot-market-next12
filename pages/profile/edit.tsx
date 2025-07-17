c46c5a064f73a1af6c76150aeb44d23022172112
Jonghwa <whdghk3471@gmail.com>
2025-07-17 13:14:11 +0900

    #11.5 Edit Profile part One

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,11 +2,39 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === "" && phone === "") {
+      setError("formErrors", {
+        message: "Email OR Phone number are required. You need to choose one.",
+      });
+    }
+  };
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="space-y-4 px-4 py-10">
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4 py-10">
         <div className="flex items-center space-x-3">
           <div className="h-14 w-14 rounded-full bg-slate-500" />
           <label
@@ -22,14 +50,26 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
         <Input
-          required
+          register={register("email")}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
+          required={false}
           label="Phone number"
           name="phone"
           type="number"
           kind="phone"
         />
+        {errors.formErrors ? (
+          <span className="my-2 block text-center font-medium text-red-500">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
         <Button text="Update profile" />
       </form>
     </Layout>
