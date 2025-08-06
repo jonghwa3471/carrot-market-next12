e830b9ea9b1fd5bba9c1277c4431089e0cf06809
Jonghwa <whdghk3471@gmail.com>
2025-08-06 17:12:32 +0900

    #17.9 Blog Section

---
+++ pages/blog.tsx
@@ -0,0 +1,9 @@
+import Layout from "@components/layout";
+
+export default function Blog() {
+  return (
+    <Layout title="Blog" seoTitle="Blog">
+      <h1 className="text-lg font-semibold">Latest Posts</h1>
+    </Layout>
+  );
+}
