c93d46c816ca127643956cbc773a7fcdc085a7a6
Jonghwa <whdghk3471@gmail.com>
2025-08-26 18:45:36 +0900

    #19.4 Parallelism

--- pages/coins.server.tsx
+++ pages/coins.server.tsx
@@ -4,15 +4,26 @@ const cache: any = {};
 
 function fetchData(url: string) {
     if(!cache[url]) {
-        throw fetch(url).then((response) => response.json()).then((json) => cache[url] = json.slice(0, 50));
+        throw Promise.all([fetch(url).then((response) => response.json()).then((json) => cache[url] = json),
+        new Promise((resolve) => setTimeout(resolve, Math.round(Math.random() * 155555)))
+    ])
     }
     return cache[url];
 }
 
+function Coin({id, name, symbol}: any) {
+    const {quotes: {USD: {price}}} = fetchData(`https://api.coinpaprika.com/v1/tickers/${id}`);
+    return <span>
+        {name} / {symbol}: ${price}
+    </span>
+}
+
 function List() {
     const coins = fetchData("https://api.coinpaprika.com/v1/coins");
-    console.log(coins);
-    return <ul>{coins.map((coin: any) => <li key={coin.id}>{coin.name} / {coin.symbol}</li>)}</ul>
+    return <div>
+        <h4>List is done</h4>
+        <ul>{coins.slice(0, 10).map((coin: any) => (<li key={coin.id}><Suspense fallback={`Coin ${coin.name} is loading...`}><Coin {...coin}/></Suspense></li>))}</ul>
+    </div>
 }
 
 export default function Coins() {
